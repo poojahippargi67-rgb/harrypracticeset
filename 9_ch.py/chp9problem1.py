2eb421eda76dc631aa4b6efcd51db79b08ae753a
Pooja <poojahippargi67@gmail.com>
2026-08-10 18:04:27 +0530

    Updated project

--- 9_ch.py/chp9problem1.py
+++ 9_ch.py/chp9problem1.py
@@ -1,39 +1,8 @@
-import random
-
-# -1 = Snake, 0 = Water, 1 = Gun
-computer = random.choice([-1, 0, 1])
-
-print("Welcome to Snake, Water, Gun Game!")
-print("Choose:")
-print("-1 for Snake")
-print(" 0 for Water")
-print(" 1 for Gun")
-
-you = int(input("Enter your choice: "))
-
-print("\nComputer chose:", computer)
-print("You chose:", you)
-
-if computer == you:
-    print("Match Draw!")
-
-elif (computer == -1 and you == 1):
-    print("You Win!")
-
-elif (computer == -1 and you == 0):
-    print("You Lose!")
-
-elif (computer == 1 and you == -1):
-    print("You Lose!")
-
-elif (computer == 1 and you == 0):
-    print("You Win!")
-
-elif (computer == 0 and you == -1):
-    print("You Win!")
-
-elif (computer == 0 and you == 1):
-    print("You Lose!")
-
+f=open("poem.txt")
+content = f.read()
+if("twinkle" in content):
+    print("twinkle is present in the content")
 else:
-    print("Invalid Input! Please enter only -1, 0, or 1.")
+    print("twinkle is present in the content")
+
+f.close()
